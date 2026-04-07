5fb5748a42978c3038ba9d2fa7bdb59f614b08ed
Rajat Khanduri <38693805+wthrajat@users.noreply.github.com>
2026-04-07 22:03:20 +0530

    feat: add `litd` with an Existing LND Node guide [AMB-2445] (#11)
    * chore: add litd with an Existing LND Node guide and restructure docs
    
    * chore: change order
    
    * chore: add lnd docs link

---
+++ content/litd/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  index: 'Overview',
+  'remote-mode': 'litd with Existing LND',
+  'existing-litd': 'Connect to Existing litd',
+  'taproot-assets': 'Taproot Assets Workflow',
+  'developer-environment': 'Developer Environment',
+};
