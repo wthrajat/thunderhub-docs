cbf3ab597bd423d95c9f574846233149151955a1
Rajat Khanduri <38693805+wthrajat@users.noreply.github.com>
2026-04-13 00:31:22 +0530

    feat: Connect a Voltage node with Thunderhub (mainnet and mutinynet) [AMB-2466] (#13)

--- content/litd/_meta.ts
+++ content/litd/_meta.ts
@@ -2,6 +2,7 @@ export default {
   index: 'Overview',
   'remote-mode': 'litd with Existing LND',
   'existing-litd': 'Connect to Existing litd',
+  voltage: 'Voltage',
   'taproot-assets': 'Taproot Assets Workflow',
   'developer-environment': 'Developer Environment',
 };
