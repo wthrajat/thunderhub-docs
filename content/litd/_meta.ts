b52ee81596a670212f60b8450300c3d18f56a3fe
Rajat Khanduri <38693805+wthrajat@users.noreply.github.com>
2026-04-20 20:55:35 +0530

    [AMB-2505] feat: add Price Oracle configuration (#14)
    * chore: add price oracle configuration
    
    * chore: address comments and PO voltage screenshot

--- content/litd/_meta.ts
+++ content/litd/_meta.ts
@@ -3,6 +3,7 @@ export default {
   'remote-mode': 'litd with Existing LND',
   'existing-litd': 'Connect to Existing litd',
   voltage: 'Voltage',
+  'price-oracle': 'Price Oracle',
   'taproot-assets': 'Taproot Assets Workflow',
   'developer-environment': 'Developer Environment',
 };
